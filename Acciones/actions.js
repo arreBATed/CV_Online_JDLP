a325028ce49137822da82961ed1d48356e8ba929
arreBATed <31872565+arreBATed@users.noreply.github.com>
2023-02-28 15:58:58 -0300

    Add files via upload

--- Acciones/actions.js
+++ Acciones/actions.js
@@ -1,32 +1,40 @@
 
 document.getElementById('boton_datos_personales').addEventListener('click', function(){
-    document.getElementById('detalle_lista_datos_personales').style.display = 'block';
-    document.getElementById('detalle_lista_datos_educacion').style.display = 'none';
-    document.getElementById('detalle_lista_datos_laborales').style.display = 'none';
-    document.getElementById('detalle_lista_referencias').style.display = 'none';
-    }
-)
+        if (document.getElementById('detalle_lista_datos_personales').style.display == 'none') {
+                document.getElementById('detalle_lista_datos_personales').style.display = 'block';
+        } else {
+                document.getElementById('detalle_lista_datos_personales').style.display = 'none';
+        }
+    })
 
 document.getElementById('boton_educacion').addEventListener('click', function(){
-    document.getElementById('detalle_lista_datos_educacion').style.display = 'block';
-    document.getElementById('detalle_lista_datos_laborales').style.display = 'none';
-    document.getElementById('detalle_lista_referencias').style.display = 'none';
-    document.getElementById('detalle_lista_datos_personales').style.display = 'none';
-    }
-)
+        if (document.getElementById('detalle_lista_datos_educacion').style.display == 'none') {
+                document.getElementById('detalle_lista_datos_educacion').style.display = 'block';
+        } else {
+                document.getElementById('detalle_lista_datos_educacion').style.display = 'none';
+        }
+    })
 
 document.getElementById('boton_experciencia').addEventListener('click', function(){
-    document.getElementById('detalle_lista_datos_laborales').style.display = 'block';
-    document.getElementById('detalle_lista_referencias').style.display = 'none';
-    document.getElementById('detalle_lista_datos_personales').style.display = 'none';
-    document.getElementById('detalle_lista_datos_educacion').style.display = 'none';
-    }
-)
+        if (document.getElementById('detalle_lista_datos_laborales').style.display == 'none') {
+                document.getElementById('detalle_lista_datos_laborales').style.display = 'block';
+        } else {
+                document.getElementById('detalle_lista_datos_laborales').style.display = 'none';
+        }
+    })
 
 document.getElementById('boton_referencia').addEventListener('click', function(){
-    document.getElementById('detalle_lista_referencias').style.display = 'block';
-    document.getElementById('detalle_lista_datos_personales').style.display = 'none';
-    document.getElementById('detalle_lista_datos_educacion').style.display = 'none';
-    document.getElementById('detalle_lista_datos_laborales').style.display = 'none';
-    }
-)
+        if (document.getElementById('detalle_lista_referencias').style.display == 'none') {
+                document.getElementById('detalle_lista_referencias').style.display = 'block';
+        } else {
+                document.getElementById('detalle_lista_referencias').style.display = 'none';
+        }
+    })
+
+    document.getElementById('boton_idiomas').addEventListener('click', function(){
+        if (document.getElementById('detalle_lista_idiomas').style.display == 'none') {
+                document.getElementById('detalle_lista_idiomas').style.display = 'block';
+        } else {
+                document.getElementById('detalle_lista_idiomas').style.display = 'none';
+        }
+    })
